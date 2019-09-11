86eb8ec6b4835b36b1e111bebd9ce8eafbb95de6
gryhan <530789565@qq.com>
2019-09-11 22:50:42 +0800

    a

---
+++ src/main/java/com/gry/myapp/test.java
@@ -0,0 +1,14 @@
+package com.gry.myapp;
+
+/**
+ * Created by HIAPAD on 2019/9/11.
+ */
+public class test {
+    public static void main(String[] args) {
+        byte[] bytes = "abc".getBytes();
+        String str = new String(bytes);
+        System.out.println("args = [" + str + "]");
+        str = "1"+"1"+"1"+"1"+"1"+"1"+"1";
+        System.out.println("args = [" + str + "]");
+    }
+}
